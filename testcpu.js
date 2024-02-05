71818097a4959bd7965c9d0fc351bcf8a9f3dc62
“Nzero” <flylusarms@gmail.com>
2024-02-05 22:09:33 +0800

    KL

--- testcpu.js
+++ testcpu.js
@@ -1,28 +1,37 @@
-function cpuMove () {
-  const randomMove = Math.random();
+let gameActive = true;
 
-  for (let i = 0; i < 3; i++) {
-    for (let j =0; j < 3; j++) {
-      if (randomMove >= 0 && randomMove <= 1/9) {
-          boardData = [
-          [-1, 0, 0],
-          [0, 0, 0],
-          [0, 0, 0],
-        ];
-      } else if (randomMove >= 1/9 && randomMove <= 2/9) {
-        boardData = [
-          [0, -1, 0],
-          [0, 0, 0],
-          [0, 0, 0],
-        ];
-      } else {
-        boardData = [
-          [0, 0, -1],
-          [0, 0, 0],
-          [0, 0, 0],
-        ]
-      }
+function cpuMove() {
+    if (!gameActive) {
+        return;
     }
-  }
+
+    const randomRow = Math.floor(Math.random() * 3);
+    const randomCol = Math.floor(Math.random() * 3);
+
+    if (boardData[randomCol][randomRow] === 0) {
+        boardData[randomCol][randomRow] = -1;
+        checkCellLeftOver();
+    } else if (boardData[randomCol][randomRow] === 1 || boardData[randomCol][randomRow] === -1) {
+        return; // Stop execution if the cell is not empty
+    }
 }
 
+let gameRunning = true;
+
+function checkCellLeftOver() {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (boardData[i][j] !== 0) {
+                gameRunning = true;
+                return;
+            } else {
+                gameRunning = false;
+                stopCpuMove(); // Call a function to stop cpuMove
+            }
+        }
+    }
+}
+
+function stopCpuMove() {
+    gameActive = false; // Set the flag to false to stop cpuMove
+}
